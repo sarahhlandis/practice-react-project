52a972b0c74c56f099f50d4b6965797d3e76b809
sarahhlandis <sarahlandis95@gmail.com>
2023-04-29 16:01:44 +1000

    header, textarea and basic button toggle complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+let noteform = <div>
+    <form id='notebox'><textarea id='textbox'></textarea></form>
+</div>
 
 function App() {
+
+  const [showForm, setShowForm] = useState(false);
+  const [buttonText, setButtonText] = useState('new note');
+
+  const handleNewNoteClick = () => {
+    setShowForm(true);
+    setButtonText('save note');
+  }
+
+  const handleSaveNoteClick = () => {
+    setShowForm(false);
+    setButtonText('new note');
+  }
+
+
   return (
-    <div className="App">
+    <div className="Notepad">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          scribble
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <button id='newNote' onClick={handleNewNoteClick}>new note</button> */}
+        {showForm && (
+          <div>
+            <button id='newNote' onClick={handleSaveNoteClick}>
+              {buttonText}
+            </button>
+            {noteform}
+          </div>
+        )}
+        {!showForm && (
+          <button id='newNote' onClick={handleNewNoteClick}>
+            {buttonText}
+          </button>
+        )}
       </header>
     </div>
   );
